99071289295cbcc62cdaccbc50ae258b2bc1a732
Hideakiii <38570881+Hideakiii@users.noreply.github.com>
2018-05-03 15:55:21 +0200

    Update Pygame_Avalonion_v3.py

--- Pygame_Avalonion_v3.py
+++ Pygame_Avalonion_v3.py
@@ -9,7 +9,7 @@ display_width = 1200
 display_height = 700
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 P1_Img = pygame.image.load('Pixelart_P1.png')
-P2_Img = pygame.image.load('Pixelart_P2.png')
+P2_Img = pygame.image.load('Pixelart_P2.jpeg')
 img_width = 75
 img_height = 75
 pygame.display.set_icon(P1_Img)
@@ -28,10 +28,12 @@ dark_brown = (90,75,80)
 light_brown = (110,95,100)
 # Punkt Komma Groß
 clock = pygame.time.Clock()
+p1_dead_start = 0
 pause = False
 player_P1 = True
 player_P2 = False
-P1_dead = False
+p1_dead = False
+p1_lives = 3
 
 #crash = True
 
@@ -88,20 +90,27 @@ def player_P2_true():
 
 def player_P2_false():
     global player_P2
-    player_P2 = False
+    player_P2 = False  
+    
+def P1_hide():
+    global p1_dead
+    global P1_x
+    global P1_y    
+    p1_dead = True
+    P1_x = -30000
+    P1_y = 20000
+    pygame.display.update()
+    
+def P1_unhide():
+    global P1_x
+    global P1_y
+    global display_height
+    global display_width
+    P1_x = display_width * 0.5
+    P1_y = display_height * 0.8
+    pygame.display.update()
+    
 
-def P1_dead(P1_x,P1_y):
-	global P1_dead
-	P1_dead = True
-	global P1_x
-	global P1_y
-	P1_x = -2000
-	P1_y = 1000
-	pygame.clock.tick(3000)
-	P1_x = (display_width * 0.5)
-	P1_y = (display_heigjt * 0.8)
-	pygame.clock.tick(3000)
-	P1_dead = False
     
 def things(thing_x, thing_y, thing_h, thing_w, color):
     pygame.draw.rect(gameDisplay, color, [thing_x, thing_y, thing_w, thing_h])
@@ -188,7 +197,12 @@ def game_intro():
 def game_loop():
     global pause
     global player_P2
-    global P1_dead
+    global player_P1
+    global p1_dead
+    global p1_dead_start
+    global P1_x
+    global P1_y
+    global p1_lives
     #variabeln_2:
     black = (0,0,0)
     white = (255,255,255)
@@ -211,6 +225,7 @@ def game_loop():
     P2_y_change = 0
     gameExit = False
     score = 0
+    
     #thing 1 :
 
     thing_start_x = random.randrange(0, display_width)
@@ -252,34 +267,44 @@ def game_loop():
     clock = pygame.time.Clock()
 
     while not gameExit:
+        if p1_dead == True:
+            p1_dead_end = time.time()
+            p1_dead_diff = p1_dead_end - p1_dead_start
+            if p1_dead_diff < 3000:
+                P1_unhide()
+            if p1_dead_diff < 6000:
+                p1_dead = False
+                
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             #player_1 1 controls:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    P1_x_change = -6.5
-                elif event.key == pygame.K_RIGHT:
-                    P1_x_change = 6.5
+            
+            if player_P1 == True and p1_dead == False:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        P1_x_change = -6.5
+                    elif event.key == pygame.K_RIGHT:
+                        P1_x_change = 6.5
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    P1_x_change = 0
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        P1_x_change = 0
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    P1_y_change = -6.5
-                elif event.key == pygame.K_DOWN:
-                    P1_y_change = 6.5
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        P1_y_change = -6.5
+                    elif event.key == pygame.K_DOWN:
+                        P1_y_change = 6.5
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    P1_y_change = 0
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        P1_y_change = 0
 
-                if event.key == pygame.K_p:
-                    pause = True
-                    paused()
+                    if event.key == pygame.K_p:
+                        pause = True
+                        paused()
             #Player 2 controls:
             if player_P2 == True:
                 if event.type == pygame.KEYDOWN:
@@ -333,11 +358,30 @@ def game_loop():
         things_4(thing_4_start_x, thing_4_start_y, thing_4_height, thing_4_width, grey)
         thing_4_start_x -= thing_4_speed
         #player_1 1:
-        if player_P1 == True and P1_dead == False:
+        if player_P1 == True and p1_dead == False:
             if P1_x > display_width - img_width or P1_x < 0:
-                crash()
+                global p1_lives
+                p1_dead = True
+                p1_dead_start = time.time()
+                P1_hide()
+                p1_lives =- 1
+                if p1_lives == 0:
+                    crash()
+                else:
+                    pass
+     
+                #crash()
             if P1_y > display_height - img_height or P1_y < 0:
-                crash()
+                p1_dead = True
+                p1_dead_start = time.time()
+                P1_hide()
+                p1_lives =- 1
+                if p1_lives == 0:
+                    crash()
+                else:
+                    pass
+               
+                #crash()
         #Player 2:
         if player_P2 == True:
             if P2_x > display_width - img_width or P2_x < 0:
@@ -365,19 +409,31 @@ def game_loop():
             thing_4_start_y = random.randrange(0, display_height)
             score += 1
         #Player 1 Kollisionen:
-        if player_P1 == True and P1_dead == False:
+        if player_P1 == True and p1_dead == False:
             if P1_y <= thing_start_y + thing_height and P1_y >= thing_start_y - thing_height:
                 if P1_x > thing_start_x and P1_x < thing_start_x + thing_width or P1_x + img_width > thing_start_x and P1_x + img_width < thing_start_x + thing_width:
-                    crash()
+                    p1_dead = True
+                    p1_dead_start = time.time()
+                    P1_hide()
+                    #crash()
             if P1_x <= thing_2_start_x + thing_2_height and P1_x >= thing_2_start_x - thing_2_height:
                 if P1_y > thing_2_start_y and P1_y < thing_2_start_y + thing_2_width or P1_y + img_width > thing_2_start_y and P1_y + img_width < thing_2_start_y + thing_2_width:
-                    crash()
+                    p1_dead = True
+                    p1_dead_start = time.time()
+                    P1_hide()
+                    #crash()
             if P1_x <= thing_3_start_x + thing_3_height and P1_x >= thing_3_start_x - thing_3_height:
                 if P1_y > thing_3_start_y and P1_y < thing_3_start_y + thing_3_width or P1_y + img_width > thing_3_start_y and P1_y + img_width < thing_3_start_y + thing_3_width:
-                    crash()
+                    p1_dead = True
+                    p1_dead_start = time.time()
+                    P1_hide()
+                    #crash()
             if P1_x <= thing_4_start_x + thing_4_height and P1_x >= thing_4_start_x - thing_4_height:
                 if P1_y > thing_4_start_y and P1_y < thing_4_start_y + thing_4_width or P1_y + img_width > thing_4_start_y and P1_y + img_width < thing_4_start_y + thing_4_width:
-                    crash()
+                    p1_dead = True
+                    p1_dead_start = time.time()
+                    P1_hide()
+                #crash()
         #Player 2 Kollisionen:
         if player_P2 == True:        
             if P2_y <= thing_start_y + thing_height and P2_y >= thing_start_y - thing_height:
@@ -405,3 +461,4 @@ game_intro()
 game_loop()
 pygame.quit()
 quit()
+
